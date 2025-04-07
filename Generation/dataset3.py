a959e91702172bf20705b7f698bf4a26c47ee1e4
Jotiraditya Sanger <113541425+Jotiraditya20@users.noreply.github.com>
2025-04-07 20:18:36 +0530

    Fixed indent

--- Generation/dataset3.py
+++ Generation/dataset3.py
@@ -215,156 +215,156 @@ def generate_research_page_N_columns(page_id, n = config["N"]):
     draw = ImageDraw.Draw(page)
 
     #Generates Rows Randomly
-        rowss = []
-        for i in range(0, n):
-            row = row_generater(PAGE_HEIGHT, config["min_row"], config["max_row"], MARGIN)
-            rowss.append(row)
+    rowss = []
+    for i in range(0, n):
+        row = row_generater(PAGE_HEIGHT, config["min_row"], config["max_row"], MARGIN)
+        rowss.append(row)
 
 
-        #Font for the page
-        fonts = config["Fonts"]
-        font_path = random.choice(fonts)
-        font_size = random.randint(config["min_font_size"], config["max_font_size"])
-        font_bold = random.randint(30, 40)
+    #Font for the page
+    fonts = config["Fonts"]
+    font_path = random.choice(fonts)
+    font_size = random.randint(config["min_font_size"], config["max_font_size"])
+    font_bold = random.randint(30, 40)
 
-        #Elememts To store all elements
-        elements = []
+    #Elememts To store all elements
+    elements = []
 
-        #cocoAnotations
-        image_id = f"page_{page_id}.jpg"
+    #cocoAnotations
+    image_id = f"page_{page_id}.jpg"
 
-        #Adding page to the coco file
-        add_image(page_count, image_id, config["PAGE_WIDTH"], config["PAGE_HEIGHT"])
+    #Adding page to the coco file
+    add_image(page_count, image_id, config["PAGE_WIDTH"], config["PAGE_HEIGHT"])
 
-        #Iterating 
-        for j in range(0, n):
-            for i in range(1, len(rowss[j])):
-                rows = rowss[j]
-            #Calculating limits and size of boxes
-                ROW_HEIGHT = rows[i] - rows[i - 1]
-                ROW_WIDTH = (PAGE_WIDTH - (2 * MARGIN) - (n*config["col_offset"]))//n
-                HEIGHT_LIMITS = [rows[i - 1], rows[i]]
+    #Iterating 
+    for j in range(0, n):
+        for i in range(1, len(rowss[j])):
+            rows = rowss[j]
+        #Calculating limits and size of boxes
+            ROW_HEIGHT = rows[i] - rows[i - 1]
+            ROW_WIDTH = (PAGE_WIDTH - (2 * MARGIN) - (n*config["col_offset"]))//n
+            HEIGHT_LIMITS = [rows[i - 1], rows[i]]
+            
+            WIDTH_LIMITS = [MARGIN + (j * ROW_WIDTH) + (j*config["col_offset"]), MARGIN + ((j+1) * ROW_WIDTH)]
+
+            #0 is picture and 2 is text
+            element_type = random.choices([0, 2], weights=config["weights"])[0]
+
+            bold = False
+            if ROW_HEIGHT < 150:
+                element_type = 2
                 
-                WIDTH_LIMITS = [MARGIN + (j * ROW_WIDTH) + (j*config["col_offset"]), MARGIN + ((j+1) * ROW_WIDTH)]
+                bold = random.choices([True, False], weights=[70, 30])[0]
+                #print("Selected Bold Text and Increased Font Size")
 
-                #0 is picture and 2 is text
-                element_type = random.choices([0, 2], weights=config["weights"])[0]
+            if element_type == 0: #Pic selected left oriented
+                width_left = ROW_WIDTH
 
-                bold = False
-                if ROW_HEIGHT < 150:
-                    element_type = 2
-                    
-                    bold = random.choices([True, False], weights=[70, 30])[0]
-                    #print("Selected Bold Text and Increased Font Size")
-
-                if element_type == 0: #Pic selected left oriented
-                    width_left = ROW_WIDTH
-
-                    if config["same_size"] == 1 and config["square"] == 1:
+                if config["same_size"] == 1 and config["square"] == 1:
+                    pic_height = random.randint(min(ROW_HEIGHT, config["min_pic_height"]), min(ROW_HEIGHT, config["max_pic_height"]))
+                    pic_width = pic_height
+                elif config["same_size"] == 1 and config["square"] != 1:
+                    pic_height = random.randint(min(ROW_HEIGHT, config["min_pic_height"]), min(ROW_HEIGHT, config["max_pic_height"]))
+                    pic_width = random.randint(min(ROW_WIDTH, config["min_pic_width"]), min(ROW_WIDTH, config["max_pic_width"]))
+                else:
+                    print("Invalid Config")
+                    break
+                #Iterating for pictures
+                while(width_left > 0):
+                    if config["same_size"] != 1 and config["square"] == 1:
                         pic_height = random.randint(min(ROW_HEIGHT, config["min_pic_height"]), min(ROW_HEIGHT, config["max_pic_height"]))
                         pic_width = pic_height
-                    elif config["same_size"] == 1 and config["square"] != 1:
+                    elif config["same_size"] != 1 and config["square"] != 1:
                         pic_height = random.randint(min(ROW_HEIGHT, config["min_pic_height"]), min(ROW_HEIGHT, config["max_pic_height"]))
                         pic_width = random.randint(min(ROW_WIDTH, config["min_pic_width"]), min(ROW_WIDTH, config["max_pic_width"]))
-                    else:
-                        print("Invalid Config")
-                        break
-                    #Iterating for pictures
-                    while(width_left > 0):
-                        if config["same_size"] != 1 and config["square"] == 1:
-                            pic_height = random.randint(min(ROW_HEIGHT, config["min_pic_height"]), min(ROW_HEIGHT, config["max_pic_height"]))
-                            pic_width = pic_height
-                        elif config["same_size"] != 1 and config["square"] != 1:
-                            pic_height = random.randint(min(ROW_HEIGHT, config["min_pic_height"]), min(ROW_HEIGHT, config["max_pic_height"]))
-                            pic_width = random.randint(min(ROW_WIDTH, config["min_pic_width"]), min(ROW_WIDTH, config["max_pic_width"]))
-                        pos_possible_vertical = ROW_HEIGHT - pic_height
-                        pos_possible_horizontal = WIDTH_LIMITS[0] + ROW_WIDTH - width_left     
-                        if config["offset"] == 1:
-                            y_pos = HEIGHT_LIMITS[0] + random.randint(0,pos_possible_vertical)
-                            x_pos = pos_possible_horizontal
-                            width_left = width_left - pic_width
-                            if width_left < 0:
-                                break
-                        elif config["offset"] == 2:
-                            print("Implementation Phase")
-                        else:
-                            y_pos = HEIGHT_LIMITS[0]
-                            x_pos = pos_possible_horizontal
-                            width_left = width_left - pic_width
-                            if width_left < 0:
-                                break
-                        #print("pic_height: " + str(pic_height))
-                        #print("pic_width: " + str(pic_width))
-                        #0 is or image and 1 is for graph
-                        element_type = element_type = random.choices([0, 1], weights=config["pic_weights"])[0]
-                        if element_type == 0:
-                            image_path = get_random_images(1, [(pic_width, pic_height)], "Generation/science_images",
-                                                    "Generation/non_science_images")[0]    
-                        else:
-                            image_path = generate_graphs(1, [(pic_width, pic_height)])[0]
-                        elements.append({
-                        "type": element_type,
-                        "path": image_path,
-                        "bbox": (x_pos, y_pos, pic_width, pic_height)
-                        })
-
-                elif element_type == 2: #Text selected
-                    #Calculation of Text boxe sizes
-                    text_height = random.randint(ROW_HEIGHT - config["min_hieght_reduce1"], ROW_HEIGHT - config["max_hieght_reduce1"])
-                    text_width = random.randint(ROW_WIDTH - config["min_width_reduce1"], ROW_WIDTH - config["max_width_reduce1"])
-                    pos_possible_vertical = ROW_HEIGHT -  text_height
-                    pos_possible_horizontal = ROW_WIDTH - text_width
+                    pos_possible_vertical = ROW_HEIGHT - pic_height
+                    pos_possible_horizontal = WIDTH_LIMITS[0] + ROW_WIDTH - width_left     
                     if config["offset"] == 1:
                         y_pos = HEIGHT_LIMITS[0] + random.randint(0,pos_possible_vertical)
-                        x_pos = WIDTH_LIMITS[0] + random.randint(0, pos_possible_horizontal)
+                        x_pos = pos_possible_horizontal
+                        width_left = width_left - pic_width
+                        if width_left < 0:
+                            break
                     elif config["offset"] == 2:
                         print("Implementation Phase")
                     else:
                         y_pos = HEIGHT_LIMITS[0]
-                        x_pos = WIDTH_LIMITS[0]
-                    try:
-                        if bold == False:
-                            image_path = generate_text_images(1, [(text_width, text_height)],font_path, font_size, bold = bold)[0]
-                        else:
-                            image_path = generate_text_images(1, [(text_width, text_height)],font_path, font_bold, bold = bold)[0]
-                    except Exception as e:
-                        print(f"Error generating Text element: {e}")
-                        continue
+                        x_pos = pos_possible_horizontal
+                        width_left = width_left - pic_width
+                        if width_left < 0:
+                            break
+                    #print("pic_height: " + str(pic_height))
+                    #print("pic_width: " + str(pic_width))
+                    #0 is or image and 1 is for graph
+                    element_type = element_type = random.choices([0, 1], weights=config["pic_weights"])[0]
+                    if element_type == 0:
+                        image_path = get_random_images(1, [(pic_width, pic_height)], "Generation/science_images",
+                                                "Generation/non_science_images")[0]    
+                    else:
+                        image_path = generate_graphs(1, [(pic_width, pic_height)])[0]
                     elements.append({
-                        "type": element_type,
-                        "path": image_path,
-                        "bbox": (x_pos, y_pos, text_width, text_height)
-                        })
+                    "type": element_type,
+                    "path": image_path,
+                    "bbox": (x_pos, y_pos, pic_width, pic_height)
+                    })
+
+            elif element_type == 2: #Text selected
+                #Calculation of Text boxe sizes
+                text_height = random.randint(ROW_HEIGHT - config["min_hieght_reduce1"], ROW_HEIGHT - config["max_hieght_reduce1"])
+                text_width = random.randint(ROW_WIDTH - config["min_width_reduce1"], ROW_WIDTH - config["max_width_reduce1"])
+                pos_possible_vertical = ROW_HEIGHT -  text_height
+                pos_possible_horizontal = ROW_WIDTH - text_width
+                if config["offset"] == 1:
+                    y_pos = HEIGHT_LIMITS[0] + random.randint(0,pos_possible_vertical)
+                    x_pos = WIDTH_LIMITS[0] + random.randint(0, pos_possible_horizontal)
+                elif config["offset"] == 2:
+                    print("Implementation Phase")
                 else:
-                    print("Invalid Type")
+                    y_pos = HEIGHT_LIMITS[0]
+                    x_pos = WIDTH_LIMITS[0]
+                try:
+                    if bold == False:
+                        image_path = generate_text_images(1, [(text_width, text_height)],font_path, font_size, bold = bold)[0]
+                    else:
+                        image_path = generate_text_images(1, [(text_width, text_height)],font_path, font_bold, bold = bold)[0]
+                except Exception as e:
+                    print(f"Error generating Text element: {e}")
+                    continue
+                elements.append({
+                    "type": element_type,
+                    "path": image_path,
+                    "bbox": (x_pos, y_pos, text_width, text_height)
+                    })
+            else:
+                print("Invalid Type")
 
-        #Adding page number
-        if config["page_number_position"] == "right":
-            x_min = PAGE_WIDTH - (100)
-            y_min = PAGE_HEIGHT - (MARGIN)
-            elements.append(generate_page_number_image(page_id, x_min, y_min, font_size= 14, font_path=font_path))
-        print(elements)
+    #Adding page number
+    if config["page_number_position"] == "right":
+        x_min = PAGE_WIDTH - (100)
+        y_min = PAGE_HEIGHT - (MARGIN)
+        elements.append(generate_page_number_image(page_id, x_min, y_min, font_size= 14, font_path=font_path))
+    print(elements)
 
-        #To Add all elements and update the coco file
-        
-        for elem in elements:
-            try:
-                img = Image.open(elem["path"])
-                page.paste(img, (elem["bbox"][0], elem["bbox"][1]))
+    #To Add all elements and update the coco file
+    
+    for elem in elements:
+        try:
+            img = Image.open(elem["path"])
+            page.paste(img, (elem["bbox"][0], elem["bbox"][1]))
 
-                # Add COCO annotation
-                add_annotation(elem_count, page_count, elem["type"], elem["bbox"])
-                elem_count = elem_count + 1
-            except Exception as e:
-                print(f"Error placing element {elem['path']}: {e}")
+            # Add COCO annotation
+            add_annotation(elem_count, page_count, elem["type"], elem["bbox"])
+            elem_count = elem_count + 1
+        except Exception as e:
+            print(f"Error placing element {elem['path']}: {e}")
 
-        # Save the page
-        page_path = os.path.join(BASE_DIR, f"page_{page_id}.jpg")
-        page.save(page_path)
-        page_count = page_count + 1
+    # Save the page
+    page_path = os.path.join(BASE_DIR, f"page_{page_id}.jpg")
+    page.save(page_path)
+    page_count = page_count + 1
 
 
-    #Adding Page No to pages
+#Adding Page No to pages
 def generate_page_number_image(page_number, x_min, y_min,font_size=30, font_path="arial.ttf", text_color="black", bg_color="white", output_dir="Generation/page"):
     text = f"Page {page_number}"
 
